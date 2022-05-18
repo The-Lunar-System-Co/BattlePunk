c31852046a2e62c6777bb753fc6d3d8a8e6e9d11
The Lunar System Company <95000093+The-Lunar-System-Co@users.noreply.github.com>
2022-05-18 15:36:01 +0400

    Update landing.tsx

--- src/pages/landing.tsx
+++ src/pages/landing.tsx
@@ -216,7 +216,7 @@ const Landing: React.FC = () => {
               className="footer__social-icons__social-3"
             />
             <HyperLink
-              href="https://raritysniper.com/nft-drops-calendar"
+              href="https://nftcalendar.io/event/battlepunk-arena/"
               className="footer__social-icons__social-4"
             />
           </div>
